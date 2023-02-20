52d5b3c26ec6b225c0eeb687d3e1fb867bcc5f80
kemilyn tinta <kemilyn1227@gmail.com>
2023-02-20 20:45:07 -0300

    aula 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while (c <=6 )
+
+
+
+/*
+var c = 1  //mais usada
+while (c <=6 ){
+    console.log(`passo ${c}`)
+    c++ //c= c + 1 , ate chegar no 6 caso contrario seria sempre 1 infinitamente
+}*/
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+ */
---
+++ aula14/ambiente.js
@@ -0,0 +1,7 @@
+console.log('vai comecar')
+
+for(var c =1 ; c<=4;c++){
+    console.log(c)
+}
+console.log('fim')
+
