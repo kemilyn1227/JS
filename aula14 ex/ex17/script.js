32514b144a27bf88caeb0e1f67b158cf29376e85
kemilyn tinta <kemilyn1227@gmail.com>
2023-02-22 13:57:28 -0300

    exercicio 17

--- aula14 ex/ex17/script.js
+++ aula14 ex/ex17/script.js
@@ -1,1 +1,50 @@
-alert ('ola')
+function gerar (){
+
+
+    let num = document.getElementById('txtn') // numero
+    let tab = document.getElementById('seltab') //selecionar
+
+    if (num.value.length ==0){
+        window.alert('por favor digite um numero')
+    }else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML =``  //limpar apos colocar outro numero
+        while( c <= 10 ){
+            let item = document.createElement('option')
+            item.text = ` ${ n} x ${c} = ${n*c}`
+            item.value = `tab${c}`//mais usado no php
+            tab.appendChild(item) //adicionar filho 
+            c++
+        }
+
+    }
+
+
+
+
+
+    /*var num = document.getElementById('txtn')  //numero
+    var res = document.getElementById('tabuada') //tabuada
+    var tab =document.getElementById('seltab')
+
+    var n =Number(num.value)
+    var t1 = 1
+
+    if (num.value.length==0){
+        var s = 0
+        for( c=0; s<=10; c+=n, s+=1){
+             
+             res.innerHTML +=`${n} x ${s} = ${c} <br>`
+        }
+    } else {
+        res.innerHTML='numero invalido'
+    }*/
+
+    
+   }
+
+   
+    
+     
+ 
